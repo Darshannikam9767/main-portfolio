bfe8ff540a56e0c2b216c8ecdef004b0c1a7ea2b
Darshannikam9767 <darshannikam64@gmail.com>
2026-08-01 14:38:51 +0530

    everything has been done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { ReactLenis } from 'lenis/react'  
+import { ReactLenis } from 'lenis/react'
 import 'lenis/dist/lenis.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -7,20 +7,54 @@ import Services from './sections/Services'
 import About from './sections/About'
 import Work from './sections/Work'
 import ContactsSummary from './sections/ContactsSummary'
+import Contact from './sections/Contact'
+import { useProgress } from '@react-three/drei'
+import { useEffect, useState } from 'react'
 
 
 const App = () => {
+
+  const { progress } = useProgress()
+  
+  const [isReady, seIsReady] = useState(false)
+
+
+  useEffect(() => {
+    if (progress === 100) {
+      seIsReady(true)
+    }
+  }, [progress])
   return (
-    <ReactLenis 
-    root 
-    className=' relative w-screen min-h-screen '>
-      <Navbar/>
-      <Hero/>
-      <ServiceSummary/>
-      <Services/>
-      <About/>
-      <Work/>
-      <ContactsSummary/>
+    <ReactLenis
+      root
+      className=' relative w-screen min-h-screen '>
+
+      {!isReady && (
+        <div className='fixed inset-0 z-999  flex flex-col items-center justify-center bg-black text-white transition-opacity duration-700 font-light'>
+          <p className='mb-4 text-xl tracking-widest animate-pulse'>
+            Loading {Math.floor(progress)} %
+          </p>
+            <div className='h-1 rounded bg-white/20 w-60  relative flex items-center'>
+              <div className='h-1.5 absolute  top-0 left-0 transition-all duration-1000 bg-white rounded' 
+              style={{
+                width:`${progress}%`
+              }} />
+            </div>
+        </div>
+      )}
+
+      <div className={`${isReady ? 'opacity-100':'opacity-0 transition-opacity duration-1000'}`}>
+        <Navbar />
+        <Hero />
+        <ServiceSummary />
+        <Services />
+        <About />
+        <Work />
+        <ContactsSummary />
+        <Contact />
+      </div>
+
+
     </ReactLenis>
   )
 }
--- portfolio/src/sections/Contact.jsx
+++ portfolio/src/sections/Contact.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
-
+import AnimatedHeaderText from '../components/AnimatedHeaderText'
+import { socials } from '../constants'
+import Marquee from '../components/Marquee'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 const Contact = () => {
-  return (
-    <section id='contact' className='min-h-screen'>
+  const text = `Got a question, how or project Idea?
+  WE'D love to hear from you and discuss further!`
+  const items = [
+    "just imagine, I code",
+    "just imagine, I code",
+    "just imagine, I code",
+    "just imagine, I code",
+    "just imagine, I code",
+  ]
 
+  useGSAP(()=>{
+    gsap.from(".social-link",{
+      y:100,
+      opacity:0,
+      delay:0.6,
+      duration:1,
+      stagger:0.3,
+      ease:"back.out",
+      scrollTrigger:{
+        trigger:".social-link"
+      }
+    })
+  },[])
+
+  return (
+    <section id='contact' className=' flex flex-col justify-between min-h-screen bg-black'>
+      <div>
+        <AnimatedHeaderText
+          subTitle={"You Dream It, I Code it"}
+          title={"Contact"}
+          text={text}
+          textColor={"text-white"}
+          withScrollTrigger={true} />
+
+        <div className='flex px-10 font-light text-white uppercase lg:text-[32px] text-[26px] leading-none mb-10'>
+          <div className='flex flex-col w-full gap-10'>
+
+            <div className='social-link'>
+              <h2>E-mail</h2>
+              <div className='w-full h-px my-2 bg-white/30' />
+              <p className='text-xl tracking-wider lowercase md:text-2xl lg:text-3xl'>
+                DarshanNikam64@gmail.com
+              </p>
+            </div>
+
+            <div className='social-link'>
+              <h2>Phone</h2>
+              <div className='w-full h-px my-2 bg-white/30' />
+              <p className='text-xl tracking-widest md:text-2xl lg:text-3xl'>
+                +91 97670 41200
+              </p>
+            </div>
+
+            <div className='social-link'>
+              <h2>Social Media</h2>
+              <div className='w-full h-px my-2 bg-white/30' />
+              <div className='flex flex-wrap gap-2'>
+                {socials.map((social, index) => (
+                  <a key={index} className='text-xs leading-loose tracking-widest uppercase md:text-sm hover:text-white/80 transition-colors duration-200' href={social.href}>{`{ ${social.name} }`}</a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Marquee  items={items} className='text-white bg-transparent' />
     </section>
   )
 }
